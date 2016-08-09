b0e7922e0df651f6286b16a95a4b4176d09eefe6
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2016-08-09 15:48:25 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # karmia-utility-random
 Karmia utility random module
+
+## Usage
+```JavaScript
+const karmia_utility_random = require('karmia-utility-random'),
+    krandom = new karmia_utility_random();
+```
+
+### string(length, options)
+- length ```<number>```
+- options ```<Object>```
+
+```JavaScript
+// Includes all characters
+utility.string.random(16);
+
+// Excludes special characters
+utility.string.random(16, {special: false});
+
+// Excludes number
+utility.string.random(16, {number: false});
+
+// Exclude upper and special characters
+utility.string.random(16, {upper: false, special: false});
+
+// Exclude lower and special characters
+utility.string.random(16, {lower: false, special: false});
+```
+
+### integer(max, min)
+- max ```<number>``` Default: Number.MAX_SAFE_INTEGER
+- min ```<number>``` Default: 0
+
+```JavaScript
+utility.random.integer(); // 0〜Number.MAX_SAFE_INTEGER
+```
---
+++ test/karmia-utility-random.js
@@ -0,0 +1,115 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/*jslint node: true, nomen: true */
+/*global describe, it */
+'use strict';
+
+
+// Variables
+const expect = require('expect.js'),
+    karmia_utility_random = require('../lib'),
+    krandom = new karmia_utility_random();
+
+
+// Test
+describe('karmia-utility-random', function () {
+    describe('string', function () {
+        describe('Should get random string', function () {
+            it('All characters', function () {
+                expect(krandom.string(32)).to.have.length(32);
+            });
+
+            it('Only numbers', function () {
+                const result = krandom.string(32, {
+                    lower: false,
+                    upper: false,
+                    special: false
+                });
+
+                expect(result).to.match(/[0-9]{32}/);
+            });
+
+            it('Only lower characters', function () {
+                const result = krandom.string(32, {
+                    number: false,
+                    upper: false,
+                    special: false
+                });
+
+                expect(result).to.match(/[a-z]{32}/);
+            });
+
+            it('Only upper characters', function () {
+                const result = krandom.string(32, {
+                    number: false,
+                    lower: false,
+                    special: false
+                });
+
+                expect(result).to.match(/[A-Z]{32}/);
+            });
+
+            it('Only characters', function () {
+                const result = krandom.string(32, {
+                    number: false,
+                    special: false
+                });
+
+                expect(result).to.match(/[A-z]{32}/);
+            });
+
+            it('Only special characters', function () {
+                const result = krandom.string(32, {
+                    number: false,
+                    lower: false,
+                    upper: false
+                });
+
+                expect(result).to.match(/[^A-z0-9]/);
+            });
+
+            it('Without characters', function () {
+                expect(krandom.string(32, {special: false})).to.match(/[A-z0-9]{32}/);
+            });
+        });
+
+        it('Should be error', function () {
+            try {
+                krandom.string(32, {
+                    number: false,
+                    lower: false,
+                    upper: false,
+                    special: false
+                });
+            } catch (e) {
+                expect(e.message).to.be('Empty haystack');
+            }
+        });
+    });
+
+    describe('integer', function () {
+        describe('Should get random integer', function () {
+            it('No parameter specified', function () {
+                const result = krandom.integer();
+                expect(Number.isInteger(result)).to.be(true);
+                expect(result).to.greaterThan(-1);
+            });
+
+            it('Max specified', function () {
+                const min = 0,
+                    max = 5,
+                    result = krandom.integer(5);
+
+                expect(result).to.greaterThan(min - 1);
+                expect(result).to.lessThan(max + 1);
+            });
+
+            it('Max and min specified', function () {
+                const min = 5,
+                    max = 10,
+                    result = krandom.integer(max, min);
+                expect(result).to.greaterThan(min - 1);
+                expect(result).to.lessThan(max + 1);
+            });
+        });
+    });
+});
